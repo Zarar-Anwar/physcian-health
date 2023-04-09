deaddbadbd7be4eb17bf1338b17597b9ba84a359
zZaala <zararanwar1234321@gmail.com>
2023-04-09 06:28:39 +0500

    Pages : Contact && About

--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -1,13 +1,98 @@
-import { CircularProgress } from "@mui/material"
+import { Link } from "react-router-dom"
 
 function Aboutus() {
   return (
     <>
-    
-    <div style={{textAlign:'center'}}>
-    <CircularProgress/>
-    <h1 >UnderConstruction</h1>
-    </div>
+    <section class="page-title-area page-title-bg1">
+            <div class="d-table">
+                <div class="d-table-cell">
+                    <div class="container">
+                        <div class="page-title-content">
+                            <h2>About Us</h2>
+                            <ul>
+                                <li><Link to="index.html">Home</Link></li>
+                                <li>About Us</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="about-area ptb-100">
+            <div class="container-fluid p-0">
+                <div class="row m-0">
+                    <div class="col-lg-6 col-md-12 p-0">
+                        <div class="about-image">
+                            <img src="assets/img/about-img1.jpg" alt=""/>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-12 p-0">
+                        <div class="about-content">
+                            <span>About Us</span>
+                            <h2>Short Story About our Medical Clinic</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+
+                            <ul>
+                                <li><i class="flaticon-check-mark"></i> Scientific Skills For getting a better result</li>
+                                <li><i class="flaticon-check-mark"></i> Communication Skills to getting in touch</li>
+                                <li><i class="flaticon-check-mark"></i> A Career Overview opportunity Available</li>
+                                <li><i class="flaticon-check-mark"></i> A good Work Environment For work</li>
+                                <li><i class="flaticon-check-mark"></i> Scientific Skills For getting a better result</li>
+                                <li><i class="flaticon-check-mark"></i> Communication Skills to getting in touch</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section class="our-vision-area ptb-100 pt-0" style={{marginTop:40}}>
+            <div class="container">
+                <div class="row">
+                <div class="our-mission-content col-lg-12 col-md-12 col-sm-12 text-center">
+                            <span class="sub-title">Our Mission &amp; Vision</span>
+                            <h2>Better Information, Better Health</h2>
+                          <p style={{fontSize:"20px"}}>"Empowering with knowledge for a healthier tomorrow"<br />
+"Transforming healthcare with informed decisions"</p>
+                        </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="single-vision-box">
+                            <div class="icon">
+                                <i class="flaticon-check-mark"></i>
+                            </div>
+
+                            <h3>Our Mission</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="single-vision-box">
+                            <div class="icon">
+                                <i class="flaticon-check-mark"></i>
+                            </div>
+
+                            <h3>Our Planning</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="single-vision-box">
+                            <div class="icon">
+                                <i class="flaticon-check-mark"></i>
+                            </div>
+
+                            <h3>Our Vision</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </>
   )
 }
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,13 +1,118 @@
-import { CircularProgress } from "@mui/material"
+import { Link } from "react-router-dom"
 
 function Contact() {
   return (
     <>
-      
-    <div style={{textAlign:'center'}}>
-    <CircularProgress/>
-    <h1 >UnderConstruction</h1>
-    </div>
+      <section class="page-title-area page-title-bg4">
+            <div class="d-table">
+                <div class="d-table-cell">
+                    <div class="container">
+                        <div class="page-title-content">
+                            <h2>Contact</h2>
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li>Contact</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="contact-area ptb-100">
+            <div class="container">
+                <div class="section-title">
+                    <span>Send Message</span>
+                    <h2>Drop us message for any query</h2>
+                    <p>If you have an idea, we would love to hear about it.</p>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-7 col-md-12">
+                        <div class="contact-form">
+                            <form id="contactForm" novalidate="true">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="name" id="name" class="form-control" required="" data-error="Please enter your name" placeholder="Name"/>
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="email" name="email" id="email" class="form-control" required="" data-error="Please enter your email" placeholder="Email"/>
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="phone_number" id="phone_number" required="" data-error="Please enter your number" class="form-control" placeholder="Phone"/>
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="msg_subject" id="msg_subject" class="form-control" required="" data-error="Please enter your subject" placeholder="Subject"/>
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-12 col-md-12">
+                                        <div class="form-group">
+                                            <textarea name="message" class="form-control" id="message" cols="30" rows="6" required="" data-error="Write your message" placeholder="Your Message"></textarea>
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-12 col-md-12">
+                                        <button type="submit" class="btn btn-primary disabled"  style={{   pointerEvents: 'all',cursor: 'pointer'}
+}>Send Message <i class="flaticon-right-chevron"></i></button>
+                                        <div id="msgSubmit" class="h3 text-center hidden"></div>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-5 col-md-12">
+                        <div class="contact-info">
+                            <ul>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fas fa-map-marker-alt"></i>
+                                    </div>
+                                    <span>Address</span>
+                                    Comsats ATD &amp; Exalters Home 
+                                </li>
+
+                                <li>
+                                    <div class="icon">
+                                        <i class="fas fa-envelope"></i>
+                                    </div>
+                                    <span>Email</span>
+                                    <a href="/contact">abc@email.com</a>
+                                    <a href="/contact">abc@email.com</a>
+                                </li>
+
+                                <li>
+                                    <div class="icon">
+                                        <i class="fas fa-phone-volume"></i>
+                                    </div>
+                                    <span>Phone</span>
+                                    <a href="/contact">+123456789</a>
+                                    <a href="/contact">+123456789</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="bg-map"><img src="assets/img/bg-map.png" alt=""/></div>
+        </section>
     </>
   )
 }
