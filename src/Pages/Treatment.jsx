64dc28e03da9bd9e681370ae1b3fda40dcd59676
zZaala <zararanwar1234321@gmail.com>
2023-04-13 02:23:20 +0500

    Updates Treatment Page

--- src/Pages/Treatment.jsx
+++ src/Pages/Treatment.jsx
@@ -7,6 +7,13 @@ import jointpain from './assets/img/blog/joint-pain.png'
 import lowbackpain from './assets/img/blog/lowbackPAin.png'
 import nerveconduction from './assets/img/blog/nerveconduction.png'
 import radiculopathy from './assets/img/blog/radiculopathy.png'
+import urology from './assets/img/blog/Urology.jpg'
+import podiatry from './assets/img/blog/podiatry.jpg'
+import neurology from './assets/img/blog/neurology.jpg'
+import physicalthraphy from './assets/img/blog/physical theraphy.jpg'
+import painmangement from './assets/img/blog/painmangemnet.jpg'
+import psychatric from './assets/img/blog/psychatric.jpg'
+
 
 function Treatment() {
   return (
@@ -128,10 +135,112 @@ At our medical clinic, we are committed to providing high-quality medical care i
 supportive environment. We invite you to explore our website to learn more about our services and to
 schedule an appointment with one of our expert healthcare professionals.</p>
                             <div class="article-image">
-                                <img src="assets/img/blog/3.jpg" alt=""/>
+                                <img style={{height:'400px'}} src={urology} alt=""/>
                             </div>
                                 <blockquote class="wp-block-quote">
                                     <p>Urology is a medical specialty that focuses on the diagnosis and treatment of diseases and conditions affecting the urinary tract and male reproductive system.</p>
+                                </blockquote>
+
+                             </div>
+                </div>
+              </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 text-center" >
+                        <div class="blog-details-desc">
+                                <h1 style={{marginBottom:"30px"}}><strong>Condition Treat By Podiatry</strong></h1>
+                                 <h6 style={{color:'#19ec67'}} ><strong>[ Arthritis, Bunions<br /><br />Diabetic foot Ulcers, Diabetic Foot Care Infection Management and Amputation Preventions<br /> <br /> Foot Deformities, Hammer Toe <br /><br />
+                                 Heel and Arc Pain/Plantar Fascilitis/Achilies Tendinitis, Injuries , Breaks and Sprains <br /><br />Neuromas , Skin and Nail Condition ]</strong></h6>
+                            <div class="article-content">
+                                <p>If you are experiencing foot or ankle pain, our podiatry specialists can help. Our team of board-certified
+podiatrists offer a range of services, including diagnostic exams, treatments for foot and ankle injuries,
+and surgical procedures. We work with patients to develop personalized treatment plans that are tailored
+to their specific needs.
+</p>
+                            <div class="article-image">
+                                <img style={{height:'400px'}} src={podiatry} alt=""/>
+                            </div>
+                                <blockquote class="wp-block-quote">
+                                    <p>Podiatry is a medical specialty that deals with the diagnosis and treatment of conditions related to the feet, ankles, and lower extremities</p>
+                                </blockquote>
+
+                             </div>
+                </div>
+              </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 text-center" >
+                        <div class="blog-details-desc">
+                                <h1 style={{marginBottom:"30px"}}><strong>Condition Treat By Neurology</strong></h1>
+                                 <h6 style={{color:'#19ec67'}} ><strong>[ Acute Spinal Cord Injury , Alzheimer's Disease<br /><br /> Bell's Palsy, Cerebral Aneurysm<br /> <br /> Brain Tumors,Epilepsy and Seizures 
+                                 <br /><br />Headache , Head Injury <br /><br />Muscular Dystrophy, Stroke (Brain Attack) <br /><br /> Migraine Heacdaches , Encephalitis <br /><br /> Septicemia , lumbar Disk Disease (Herniated Disk) <br /><br />Diabetic Neuropathy ]</strong></h6>
+                            <div class="article-content">
+                                <p>Our neurology specialists provide expert care for patients with neurological conditions, including
+migraines, seizures, multiple sclerosis, and Parkinson's disease. Our team of board-certified neurologists
+uses the latest diagnostic tools and treatments to help patients manage their conditions and improve their
+quality of life.
+</p>
+                            <div class="article-image">
+                                <img style={{height:'400px'}} src={neurology} alt=""/>
+                            </div>
+                                <blockquote class="wp-block-quote">
+                                    <p>Neurology is a medical specialty that deals with disorders of the nervous system, including the brain, spinal cord, and nerves.</p>
+                                </blockquote>
+
+                             </div>
+                </div>
+              </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 text-center" >
+                        <div class="blog-details-desc">
+                                <h1 style={{marginBottom:"30px"}}><strong>Condition Treat By Physical Theraphy</strong></h1>
+                                 <h6 style={{color:'#19ec67'}} ><strong>[ Pain theraphy , Sports injuries<br /><br /> joint Health , Nerve Injuries <br /> <br /> General Physcial Medicine , Congenital Conditions <br /><br /> Bone Health, Brain Disorders ]</strong></h6>
+                            <div class="article-content">
+                                <p>Our physical therapy services are designed to help patients improve their strength, mobility, and overall
+physical function. Our licensed physical therapists work with patients of all ages and abilities to develop
+customized treatment plans that include exercises, stretches, and other techniques to help patients
+recover from injury or surgery, manage chronic conditions, and improve overall physical health.
+</p>
+                            <div class="article-image">
+                                <img style={{height:'400px'}} src={physicalthraphy} alt=""/>
+                            </div>
+                                <blockquote class="wp-block-quote">
+                                    <p>Physical therapy is a healthcare specialty that focuses on the treatment of individuals with physical impairments, disabilities, or pain.</p>
+                                </blockquote>
+
+                             </div>
+                </div>
+              </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 text-center" >
+                        <div class="blog-details-desc">
+                                <h1 style={{marginBottom:"30px"}}><strong>Condition Treat By Pain Management</strong></h1>
+                                 <h6 style={{color:'#19ec67'}} ><strong>[ Abdominal Pain , Angina ,Arthritis <br /><br /> Back pain , Calciphylaxis , Chronic Pain<br /> <br /> Acute pain , Chronic Pelvic Pain ,Chronic Pelvic Pain in women
+                                 <br /><br /> Complex Regional Pain Syndrome , Costochondritis <br /><br /> Muscle Pain All Over , Disk Disease <br /><br /> Facet Joint Syndrome , Failed back syncdrome <br /><br /> Foot pain , Joint Pain , Leg Pain <br /><br /> Myofascial Pain syndrome , Neck pain <br /> 
+                                 <br /> Neuralgia , Occipital Neuralgia , Pancreatitis <br /><br /> Peripheral neuropathy , Post-hernia surgery pain <br /><br /> Postherpetic neuralgia , post-knww surgery chronic pain <br /><br /> Sacroiliac pain syndromes , Spinal Pain , Spinal Stenosis <br /><br /> Sports injuries ,Work-realted injury ]</strong></h6>
+                            <div class="article-content">
+                                <p>If you are experiencing chronic pain, our pain management specialists are here to help. Our team of
+board-certified pain management doctors offer a range of treatment options, including medications,
+injections, physical therapy, and more. We will work with you to develop a personalized pain management
+plan that is tailored to your specific needs.
+</p>
+                            <div class="article-image">
+                                <img style={{height:'400px'}} src={painmangement} alt=""/>
+                            </div>
+                                <blockquote class="wp-block-quote">
+                                    <p>Pain management is a medical specialty that focuses on the evaluation, diagnosis, and treatment of chronic pain. </p>
+                                </blockquote>
+
+                             </div>
+                </div>
+              </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 text-center" >
+                        <div class="blog-details-desc">
+                                <h1 style={{marginBottom:"30px"}}><strong>Condition Treat By Psychatric</strong></h1>
+                                 <h6 style={{color:'#19ec67'}} ><strong>[ Alcohol use Disorders and Other, Alzheimer's Disease <br /><br /> Anxiety disorders , Attention-deflict/hyperactivity  <br /> <br /> Autism spectrum disorder , Bipolar disorder , body dysmorphic disorder <br /><br />
+                                 Depression , Dissociative disorder , Eating Disorder <br /><br /> Gambling Disorder , Gender dysporia , hoarding disorder <br /><br /> Mood Disorder , Obsessive compulsive disorder <br /><br /> Panic Disorder , Personality Disorder 
+                                 <br /><br /> Post traumatic stress Disorder , Postpartum Depression  <br /> <br /> Schizoaffective disorder , schizophrenia , sleep disorder ]</strong></h6>
+                            <div class="article-content">
+                                <p>Psychiatry is a medical specialty that focuses on the diagnosis, treatment, and prevention of mental illnesses and disorders. Psychiatrists are medical doctors who specialize in mental health and are trained to evaluate and diagnose patients with a wide range of mental health conditions. They use a combination of psychotherapy, medication, and other interventions to treat conditions such as depression, anxiety, bipolar disorder, schizophrenia, and substance abuse disorders. Psychiatry is a critical field in the medical community, as mental health conditions can significantly impact a person's quality of life and their ability to function in their daily lives.</p>
+                            <div class="article-image">
+                                <img style={{height:'400px'}} src={psychatric} alt=""/>
+                            </div>
+                                <blockquote class="wp-block-quote">
+                                    <p>Psychiatry is a branch of medicine that deals with the diagnosis, treatment, and prevention of mental health disorders. </p>
                                 </blockquote>
 
                              </div>
